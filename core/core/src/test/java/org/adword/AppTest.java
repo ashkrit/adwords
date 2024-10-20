a513451c45992db9c9a6b2313fb8e82f788e01e3
ashkrit@gmail.com <ashkrit@gmail.com>
2024-10-20 13:49:47 +0800

    Initial Project

---
+++ core/core/src/test/java/org/adword/AppTest.java
@@ -0,0 +1,20 @@
+package org.adword;
+
+
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+
+    @Test
+    public void testApp() {
+        assertEquals(1, 1);
+    }
+
+
+}
